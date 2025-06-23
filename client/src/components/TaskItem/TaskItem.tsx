5dab56eb0e686033ed18dcbe4ac3430750354af5
becca221b <lrpmqrp@gmail.com>
2025-06-23 19:05:41 -0300

    add TaskItem

---
+++ client/src/components/TaskItem/TaskItem.tsx
@@ -0,0 +1,42 @@
+import { Card, Button } from 'react-bootstrap';
+import type { Task } from '../../types/Task';
+import './TaskItem.css';
+
+interface TaskItemProps {
+  task: Task;
+  onToggle: (id: string, completed: boolean) => void;
+  onDelete: (id: string) => void;
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({ task, onToggle, onDelete }) => {
+    return(
+        <Card className="task-card">
+            <div className="task-horizontal">
+                <span className={`task-title ${task.completed ? 'completed' : ''}`}>
+                    {task.title}
+                </span>
+
+                <div className="task-buttons">
+                    <Button
+                        variant="outline-secondary"
+                        size="sm"
+                        className={`rounded-circle icon-btn ${task.completed ? 'check-green' : ''}`}
+                        onClick={() => onToggle(task._id, task.completed)}
+                    >
+                        {task.completed ? '☑' : '☐'}
+                    </Button>
+
+                    <Button
+                        variant="dark"
+                        size="sm"
+                        className="rounded-circle icon-btn"
+                        onClick={() => onDelete(task._id)}
+                        >
+                        🗑
+                    </Button>
+                </div>
+                    </div>
+        </Card>
+    );
+};
+export default TaskItem;
--- client/src/components/TasksList/TaskList.tsx
+++ client/src/components/TasksList/TaskList.tsx
@@ -1,5 +1,6 @@
 import type {Task} from '../../types/Task';
-import { Card, Row, Col, Button } from 'react-bootstrap';
+import TaskItem from '../TaskItem/TaskItem';
+
 import './TaskList.css';
 
 interface TaskListProps {
@@ -11,52 +12,16 @@ interface TaskListProps {
 const TaskList: React.FC<TaskListProps> = ({ tasks, onToggle, onDelete }) => {
     return(
         <div className="todos">
-            {(tasks.length > 0) &&
-            tasks.map((task) => (
-                <Card key={task._id} className="mb-3 shadow-sm">
-            <Card.Body>
-              <Row className="align-items-center">
-                <Col xs={8}>
-                  <Card.Title
-                    style={{
-                      textDecoration: task.completed ? 'line-through' : 'none',
-                      cursor: 'pointer',
-                      marginBottom: 0
-                    }}
-                    onClick={() => onToggle(task._id, task.completed)}
-                  >
-                    {task.title}
-                  </Card.Title>
-                  <Card.Text className="text-muted">
-                    {task.description}
-                  </Card.Text>
-                </Col>
-
-                <Col xs={4} className="text-end">
-                  <Button
-                    variant={task.completed ? 'success' : 'outline-secondary'}
-                    size="sm"
-                    className="me-2"
-                    onClick={() => onToggle(task._id, task.completed)}
-                  >
-                    {task.completed ? '☑' : '☐'}
-                  </Button>
-
-                  <Button
-                    variant="danger"
-                    size="sm"
-                    onClick={() => onDelete(task._id)}
-                  >
-                    🗑
-                  </Button>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-                
-          ))}
-      </div>
-    )
-}
+            {tasks.map((task) => (
+              <TaskItem
+                key={task._id}
+                task={task}
+                onToggle={onToggle}
+                onDelete={onDelete}
+              />
+            ))}
+        </div>
+    );
+};
 
 export default TaskList;
