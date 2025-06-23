350f8a7885f4d8c1552dfb6eece69a03d16ba6a0
becca221b <lrpmqrp@gmail.com>
2025-06-23 20:46:42 -0300

    Added TaskFilter

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import TaskForm from "../components/TaskForm/TaskForm";
 import TaskList from "../components/TasksList/TaskList";
+import TaskFilter from "../components/TaskFilter/TaskFilter";
 import type { Task } from "../types/Task";
 
 const MainPage: React.FC = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [filter, setFilter] = useState<string>("");
 
     const fetchTasks = async () =>{
         const res = await fetch('http://localhost:5000/api/tasks');
+        if(filter === "completed") {
+            const data = await res.json();
+            setTasks(data.tasks.filter((task: Task) => task.completed));
+            return;
+        }else if(filter === "pending") {
+            const data = await res.json();
+            setTasks(data.tasks.filter((task: Task) => !task.completed));
+            return;
+        }
         const data = await res.json();
         setTasks(data.tasks);
     }
 
     useEffect(()=>{
         fetchTasks();
-    }, []);
+    }, [filter]);
 
     const createTask = async(title: string, description: string)=>{
         const res = await fetch('http://localhost:5000/api/tasks',{
@@ -49,6 +60,7 @@ const MainPage: React.FC = () => {
         <div className="container">
             <h1 className="text-center mb-4">App de Tareas</h1>
             <TaskForm onCreate={createTask}/>
+            <TaskFilter filter={filter} onChange={setFilter} />
             <TaskList tasks={tasks} onToggle={toggleTask} onDelete={deleteTask} />
         </div>
     );
