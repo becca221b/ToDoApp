018835452ca99ce3dea9fe5bce69e950d9927511
becca221b <lrpmqrp@gmail.com>
2025-06-24 17:54:54 -0300

    Added validations

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: { type: String, required: [true, "title  is required"] },
-    description: String,
+    title: { 
+        type: String, 
+        required: [true, "title  is required"] },
+    description: {
+        type: String,
+        maxlength: [50, "La descripción no puede tener más de 50 caracteres"]
+    },
     completed: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now }
 },{ timestamps: true });
